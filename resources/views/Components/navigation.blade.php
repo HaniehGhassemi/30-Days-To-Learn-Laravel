5b1f6a436aad1e72bb4d31576db7e202cb87bc17
Hanieh Ghassemi <haniehghsmie@gmail.com>
2025-05-13 15:09:12 +0330

    Initial commit

---
+++ resources/views/Components/navigation.blade.php
@@ -0,0 +1,5 @@
+<nav>
+    <x-nav-link href="/">Home</x-nav-link>
+    <x-nav-link href="/about" style="color:chartreuse;">About</x-nav-link>
+    <x-nav-link href="/contact">Contact</x-nav-link>
+</nav>
